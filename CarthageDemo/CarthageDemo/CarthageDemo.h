5c365f759b1dcfea67786ef31da2f143a56ec54d
Mohammad Parvez <m.parvez@unlimint.com>
2023-10-19 11:00:37 +0530

    InitialCommit

---
+++ CarthageDemo/CarthageDemo/CarthageDemo.h
@@ -0,0 +1,18 @@
+//
+//  CarthageDemo.h
+//  CarthageDemo
+//
+//  Created by Mohammad Parvez on 19/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarthageDemo.
+FOUNDATION_EXPORT double CarthageDemoVersionNumber;
+
+//! Project version string for CarthageDemo.
+FOUNDATION_EXPORT const unsigned char CarthageDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageDemo/PublicHeader.h>
+
+
